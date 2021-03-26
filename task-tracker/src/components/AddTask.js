3cee939560cd6ec1ef8b6aa90e44c3555f171553
Aryamaan <pandeyaryamaan@gmail.com>
2021-03-26 15:01:33 +0530

    Form input state+Controlled Components

--- task-tracker/src/components/AddTask.js
+++ task-tracker/src/components/AddTask.js
@@ -1,19 +1,30 @@
+// eslint-disable-next-line
+import {useState} from 'react'
+
+
 const AddTask = () => {
+    // eslint-disable-next-line
+    const [text,setText]=useState('')
+     // eslint-disable-next-line
+    const [day,setDay]=useState('')
+     // eslint-disable-next-line
+    const [reminder,setReminder]=useState(false)
+
     return (
         <form className='add-form'>
             <div className='form-control'>
                 <label>Task</label>
-                <input type='text' placeholder='Add Task'></input>
+                <input type='text' placeholder='Add Task' value={text} onChange={(e)=>setText(e.target.value)}></input>
             </div>
 
             <div className='form-control'>
                 <label>Day & Time</label>
-                <input type='text' placeholder='Day & Time'></input>
+                <input type='text' placeholder='Day & Time' value={day} onChange={(e)=>setDay(e.target.value)}></input>
             </div>
 
             <div className='form-control form-control-check'>
                 <label>Set Reminder</label>
-                <input type='checkbox'></input>
+                <input type='checkbox' onChange={(e)=>setReminder(e.currentTarget.checked)}></input>
             </div>
 
             
