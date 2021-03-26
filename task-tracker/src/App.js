8dcf3de0ccbbbd8573037d9117778639af580adc
Aryamaan <pandeyaryamaan@gmail.com>
2021-03-26 10:27:14 +0530

    Component-Props

--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -1,13 +1,19 @@
 //eslint-disable-next-line
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Header from './components/Header'
 
 function App() {
+  // eslint-disable-next-line 
+  const name='Aryamaan'
+  // eslint-disable-next-line
+  const x=true
   return (
-    <div className="App">
-      <h1>Hello From React</h1>
-      
+    <div className="container">
+      <Header />
     </div>
+    
   );
 }
 
