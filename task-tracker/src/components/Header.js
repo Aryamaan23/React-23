c8e224fa57255fba142fba16a832e4e874fedb10
Aryamaan <pandeyaryamaan@gmail.com>
2021-03-26 10:51:51 +0530

    Component:Styling

--- task-tracker/src/components/Header.js
+++ task-tracker/src/components/Header.js
@@ -1,7 +1,11 @@
+import PropTypes from 'prop-types'
+
+
 const Header = ({title}) => {
     return (
-       <header>
-           <h1>{title}</h1>
+       <header className='header'>
+           <h1 >{title}</h1>
+           <button className='btn'>Add</button>
        </header>
     )
 }
@@ -9,5 +13,15 @@ const Header = ({title}) => {
 Header.defaultProps={
     title:'Task Tracker'
 }
+
+Header.propTypes={
+    title:PropTypes.string.isRequired,
+}
+
+//CSS in Js
+//const headingStyle={
+   // color:'red',
+   //backgroundColor:'black'
+//}
 
 export default Header
